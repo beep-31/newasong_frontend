835950d41da30ca98e59d8cbe1d5b4ba21ce7eee
unknown <ascheglov17@gmail.com>
2020-10-23 19:54:53 +0200

    Navbar and header were modified

--- js/script.js
+++ js/script.js
@@ -1,16 +1,17 @@
-let navbarOpen = document.querySelector('#open'),
-    navbarClose = document.querySelector('#close'),
-    navbar = document.querySelector('.mobile-navbar');
-console.log(navbar);
-navbarOpen.addEventListener('click', function(){
-    if(navbar.style.right =='-320px'){ 
+let burger = document.querySelector('#burger'),
+    navbar = document.querySelector('.mobile-navbar'),
+    burger_item = document.querySelector('#burger-item'),
+    opened = 'burger__item--opened',
+    closed = 'burger__item--closed';
+burger.addEventListener('click', function(){
+    console.log('CLICKED!');
+    if(navbar.style.right=='-320px'){ 
         navbar.style.right='0px';
+        burger_item.classList.remove(closed);
+        burger_item.classList.add(opened);
+    } else {
+        navbar.style.right = '-320px';
+        burger_item.classList.remove(opened);
+        burger_item.classList.add(closed);
     }
 });
-
-navbarClose.addEventListener('click', function() {
-    if(navbar.style.right =='0px'){
-        navbar.style.right = '-320px';
-    } 
-});
-
