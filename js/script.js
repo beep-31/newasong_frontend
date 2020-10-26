95331ad2c7915aa424fcd8a2614fd09284e7fa63
beep-31 <alex.scheglov25@gmail.com>
2020-10-26 17:24:31 +0100

    Some styles and js script were modified

--- js/script.js
+++ js/script.js
@@ -15,3 +15,35 @@ burger.addEventListener('click', function(){
         burger_item.classList.add(closed);
     }
 });
+
+let email_input = document.querySelector('#input__email'),
+    email_label = document.querySelector('#email__label');
+
+email_input.addEventListener('focusout', function(){
+    if(!email_input.checkValidity()){
+        email_input.style.borderColor = 'red';
+        email_label.style.color = 'red';
+        email_label.style.top = '-10px';
+        email_label.style.fontSize = '10px';
+    } else {
+        email_input.style.borderColor = '#f3f3f3';
+        email_label.style.color = '#f3f3f3';
+        email_label.style.top = '0px';
+        email_label.style.fontSize = '14px';
+    }
+
+    if(email_input.value == ''){
+        email_input.style.borderColor = '#f3f3f3';
+        email_label.style.color = '#f3f3f3';
+        email_label.style.top = '0px';
+        email_label.style.fontSize = '14px';
+    }
+});
+
+email_input.addEventListener('focus', function(){
+    email_input.style.borderColor = 'rgba(255, 123, 0, 0.9)';
+    email_label.style.color = '#f3f3f3';
+    email_label.style.fontSize = '10px';
+    email_label.style.top = '-10px';
+
+});
